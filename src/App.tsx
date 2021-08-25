8d30f0f4a7332209ea0ff834f9bd35760a7c23e4
Juan Cortez <juan_cortez@utexas.edu>
2021-08-25 09:40:48 -0500

    Create sudoku boxes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sudoku from './components/Sudoku';
+
 function App() {
   return (
     <div className="App">
@@ -10,14 +12,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <Sudoku />
       </header>
     </div>
   );
---
+++ src/components/Sudoku.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sudoku.css';
+
+const Sudoku = () => {
+  return (
+    <section>
+      <h1>Sudoku</h1>
+      <div className="sudoku-box"></div>
+      <div className="sudoku-box"></div>
+    </section>
+  );
+};
+
+export default Sudoku;
