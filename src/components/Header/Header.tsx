40578fea8869a3cc4a9e6af016a1b426b36b4d60
Juan Cortez <juan_cortez@utexas.edu>
2021-08-26 11:13:30 -0500

    Add banner component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="landing-page-banner">
+      <h1 className="landing-page-banner-title">Mintbean Game Catalog</h1>
+
+      <h3 className="landing-page-banner-intro">Welcome to the amazing world of Mintbean Hackathon Gaming!</h3>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
+import './fonts/PressStart/PressStart.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
