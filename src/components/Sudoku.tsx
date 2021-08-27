5d1a998523e720349f4cd0e0f22c74e3383686c0
Paige Morton <paigeashley624@gmail.com>
2021-08-27 18:21:42 -0400

    adds number selector box. needs to be fixed still but it appears now

--- src/components/Sudoku.tsx
+++ src/components/Sudoku.tsx
@@ -4,15 +4,15 @@ import "./Sudoku.scss";
 function SudokuItem() {
   return (
     <div className="game-board">
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
-      <div className="box">9</div>
+      <div className="box"></div>
+      <div className="box">1</div>
+      <div className="box"></div>
+      <div className="box">4</div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box">5</div>
+      <div className="box"></div>
     </div>
   );
 }
@@ -32,26 +32,29 @@ function Sudoku() {
     <header className="sudoku-page-banner">
       {/* need to get this aligned side by side */}
       <h1 className="sudoku-page-banner-title">Sudoku</h1>
-      <h4 className="sudoku-level-selector" style={{ color: "white" }}>
-        Easy
-      </h4>
-      <h4 className="sudoku-level-selector" style={{ color: "white" }}>
-        Medium
-      </h4>
-      <h4 className="sudoku-level-selector" style={{ color: "blue" }}>
-        Hard
-      </h4>
+      <div className="sudoku-level-selector-container">
+        <h4 className="sudoku-level-selector" style={{ color: "white" }}>
+          Easy
+        </h4>
+        <h4 className="sudoku-level-selector" style={{ color: "white" }}>
+          Medium
+        </h4>
+        <h4 className="sudoku-level-selector" style={{ color: "blue" }}>
+          Hard
+        </h4>
+      </div>
 
       <section>
         <SudokuRow />
         <SudokuRow />
         <SudokuRow />
       </section>
+      {/* <h4 className="sudoku-level-selector" style={{ color: "white" }}>
+        Reset
+      </h4> */}
       <br></br>
       <section>
-        <NumberRow />
-        <NumberRow />
-        <NumberRow />
+        <NumberSelector />
       </section>
     </header>
   );
@@ -61,28 +64,26 @@ function Sudoku() {
 
 function NumberSelector() {
   return (
-    <div className="number-selector">
-      <div className="box">1</div>
-      <div className="box">2</div>
-      <div className="box">3</div>
-      <div className="box">4</div>
-      <div className="box">5</div>
-      <div className="box">6</div>
-      <div className="box">7</div>
-      <div className="box">8</div>
-      <div className="box">9</div>
-    </div>
+    <ul className="flex-container">
+      <li className="flex-item">1</li>
+      <li className="flex-item">2</li>
+      <li className="flex-item">3</li>
+      <li className="flex-item">4</li>
+      <li className="flex-item">5</li>
+      <li className="flex-item">6</li>
+    </ul>
   );
 }
 
-const NumberRow = () => {
-  return (
-    <div className="number-container">
-      {[1, 2, 3].map((_, i) => (
-        <SudokuItem key={i} />
-      ))}
-    </div>
-  );
-};
+// const NumberRow = () => {
+//   return (
+//     <div className="number-container">
+//       {[1].map((_, i) => (
+//         <NumberSelector key={i} />
+//       ))}
+//     </div>
+//   );
+// };
 
+// NumberSelector;
 export default Sudoku;
