220f356f76c0ef93592eb177531cf08c0d84a185
Juan Cortez <juan_cortez@utexas.edu>
2021-08-29 12:34:14 -0500

    sudoku user board update and difficulty updates

--- src/components/Sudoku.tsx
+++ src/components/Sudoku.tsx
@@ -1,23 +1,54 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import SudokuGenerator from "./../lib/SudokuGame";
-import "./Sudoku.scss";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import SudokuGenerator from './../lib/SudokuGame';
+import './Sudoku.scss';
+
+enum Difficulty {
+  EASY,
+  MEDIUM,
+  HARD,
+}
+const defaultLevel = Difficulty.HARD;
 
 /**
  *
  * Percent chance that a number is visible
  */
-const randomIsVisible = () => {
+const randomIsVisible = (level: Difficulty) => {
   const rand = Math.floor(Math.random() * 10);
 
-  return rand <= 2;
+  switch (level) {
+    case Difficulty.EASY:
+      return rand <= 8;
+    case Difficulty.MEDIUM:
+      return rand <= 5;
+    case Difficulty.HARD:
+      return rand <= 2;
+    default:
+      return rand <= 2;
+  }
 };
 
 interface SudokuRowProps {
   data: number[];
-  row: string;
+  row: number;
+  level: Difficulty;
 }
 
-const SudokuRow = ({ data, row }: SudokuRowProps) => {
+const SudokuRow = ({ data, row, level }: SudokuRowProps) => {
+  const [originalRowData, setOriginalRowData] = useState<number[]>();
+
+  useEffect(() => {
+    if (!originalRowData) {
+      setOriginalRowData([...data]);
+    }
+  }, [originalRowData, data]);
+
+  useEffect(() => {
+    if (level !== defaultLevel) {
+      setOriginalRowData([...data]);
+    }
+  }, [level, data]);
+
   if (!Array.isArray(data)) {
     return null;
   }
@@ -25,17 +56,9 @@ const SudokuRow = ({ data, row }: SudokuRowProps) => {
   return (
     <div className="box-row">
       {data.map((num, i) => {
-        const isVisible = randomIsVisible();
-
         return (
-          <div
-            key={`row-${row} column-${i}`}
-            data-column={i}
-            data-row={row}
-            data-correct-result={num}
-            className={`box ${isVisible ? "box-is-visible" : ""}`}
-          >
-            {isVisible ? num : ""}
+          <div key={`row-${row} column-${i}`} data-column={i} data-row={row} className={`box ${originalRowData?.[i] === -1 ? '' : 'box-is-visible'}`}>
+            {num === -1 ? '' : num}
           </div>
         );
       })}
@@ -45,36 +68,75 @@ const SudokuRow = ({ data, row }: SudokuRowProps) => {
 
 function Sudoku() {
   const sudokuGame = useRef(new SudokuGenerator());
-  const selectedItem = useRef();
+  const selectedItem = useRef<HTMLElement>();
   const [board, setBoard] = useState<number[][]>([]);
   const [userBoard, setUserBoard] = useState<number[][]>([]);
+  const [attempts, setAttempts] = useState(0);
+  const [userWon, setUserWon] = useState<boolean>(false);
+  const [level, setLevel] = useState<Difficulty>(defaultLevel);
+
+  useEffect(() => {
+    if (userBoard.length) {
+      const isValid = sudokuGame.current.isSolutionValid(userBoard);
+
+      if (isValid) {
+        setUserWon(true);
+      }
+    }
+  }, [attempts, userBoard, sudokuGame]);
+
+  useEffect(() => {
+    if (level !== defaultLevel) {
+      setUserWon(false);
+      setUserBoard([]);
+      const board = sudokuGame.current.create();
+      setBoard(board);
+    }
+  }, [level]);
 
   useEffect(() => {
     const board = sudokuGame.current.create();
     setBoard(board);
-    // [] is when a component mounts
   }, []);
+
+  useEffect(() => {
+    const tmpBoard: number[][] = [];
+
+    if (board.length) {
+      for (let row = 0; row < board.length; row++) {
+        for (let column = 0; column < board[row].length; column++) {
+          tmpBoard[row] = tmpBoard[row] ?? [];
+          tmpBoard[row][column] = randomIsVisible(level) ? board[row][column] : -1;
+        }
+      }
+      setUserBoard(tmpBoard);
+    }
+  }, [level, board]);
 
   const onBoardClick = useCallback((evt) => {
     const target = evt.target;
     selectedItem.current = target;
+
+    if (selectedItem.current) {
+      selectedItem.current.classList.add('highlighted');
+    }
   }, []);
 
   const onItemClick = useCallback(
-    (evt) => {
+    (evt: any) => {
       const target = evt.target;
-      // @ts-ignore
-      const column = selectedItem.current.getAttribute("data-column");
-      // @ts-ignore
-      const row = selectedItem.current.getAttribute("data-column");
+      const column = selectedItem.current?.getAttribute('data-column') as string;
+      const row = selectedItem.current?.getAttribute('data-row') as string;
 
-      console.log(`The following item was selected row ${row} and column ${column}`);
-
-      userBoard[row][column] = +target.innerText;
-      // @ts-ignore
-      selectedItem.current.innerHTML = target.innerText;
+      if (column && row) {
+        setUserBoard((b) => {
+          b[+row][+column] = +target?.innerText;
+          return b;
+        });
+        setAttempts((c) => c + 1);
+      }
     },
-    [userBoard, selectedItem]
+    [selectedItem, setUserBoard]
   );
 
   return (
@@ -82,34 +144,29 @@ function Sudoku() {
       {/* need to get this aligned side by side */}
       <h1 className="sudoku-page-banner-title">Sudoku</h1>
       <div className="sudoku-level-selector-container">
-        <h4 className="sudoku-level-selector" style={{ color: "white" }}>
+        <h4 className={`sudoku-level-selector ${level === Difficulty.EASY ? 'sudoku-level-selected' : ''}`} onClick={() => setLevel(Difficulty.EASY)}>
           Easy
         </h4>
-        <h4 className="sudoku-level-selector" style={{ color: "white" }}>
+        <h4 className={`sudoku-level-selector ${level === Difficulty.MEDIUM ? 'sudoku-level-selected' : ''}`} onClick={() => setLevel(Difficulty.MEDIUM)}>
           Medium
         </h4>
-        <h4 className="sudoku-level-selector" style={{ color: "blue" }}>
+        <h4 className={`sudoku-level-selector ${level === Difficulty.HARD ? 'sudoku-level-selected' : ''}`} onClick={() => setLevel(Difficulty.HARD)}>
           Hard
         </h4>
       </div>
 
       <section onClick={onBoardClick}>
-        <SudokuRow row="0" data={board[0]} />
-        <SudokuRow row="1" data={board[1]} />
-        <SudokuRow row="2" data={board[2]} />
-        <SudokuRow row="3" data={board[3]} />
-        <SudokuRow row="4" data={board[4]} />
-        <SudokuRow row="5" data={board[5]} />
-        <SudokuRow row="6" data={board[6]} />
-        <SudokuRow row="7" data={board[7]} />
-        <SudokuRow row="8" data={board[8]} />
+        {userBoard.map((_, index) => {
+          return <SudokuRow level={level} key={index} row={index} data={userBoard[index]} />;
+        })}
       </section>
+
+      {userWon && <h2 className="sudoku-win">You win!</h2>}
       {/* <h4 className="sudoku-level-selector" style={{ color: "white" }}>
         Reset
       </h4> */}
       <br></br>
       <section>
-        return (
         <ul className="flex-container" onClick={onItemClick}>
           <li className="flex-item">1</li>
           <li className="flex-item">2</li>
